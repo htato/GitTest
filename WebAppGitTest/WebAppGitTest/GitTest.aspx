4ca2ad8a88129213a028e17f902652da68355e6a
htato <helder.tato@3ctechlab.com>
2017-05-15 16:41:40 +0100

    version 1.0

---
+++ WebAppGitTest/WebAppGitTest/GitTest.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GitTest.aspx.cs" Inherits="WebAppGitTest.GitTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Button ID="btnStart" runat="server" OnClick="btnStart_Click" Text="Start" />
+        <br />
+        <br />
+        <br />
+        <asp:CheckBox ID="cbTest" runat="server" />
+    </form>
+</body>
+</html>
---
+++ WebAppGitTest/WebAppGitTest/GitTest.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppGitTest
+{
+    public partial class GitTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                cbTest.Checked = false;
+                cbTest.Text = "Unchecked";
+            }
+
+        }
+
+        protected void btnStart_Click(object sender, EventArgs e)
+        {
+            if (cbTest.Text == "Checked")
+            {
+                cbTest.Text = "Unchecked";
+                cbTest.Checked = false;
+            }
+            else
+            {
+                cbTest.Text = "Checked";
+                cbTest.Checked = true;
+             }
+        }
+    }
+}
